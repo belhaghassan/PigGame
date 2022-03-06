57f0397afd1a0cccdb59e2a64e1085ce1b3f6dcf
belhaghassan <51680128+belhaghassan@users.noreply.github.com>
2022-03-05 23:37:02 -0800

    Update __init__.py

--- pig/pig_game/__init__.py
+++ pig/pig_game/__init__.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+# Bilal El-haghassan
+# CPSC 386-04
+# 2022-02-28
+# bilalelhaghassan@csu.fullerton.edu
+# @belhaghassan
+#
+# Lab 00-02
+#
+# This is PigGame, a game of DICE and Chance
+#
+
+"""Dice class for our Pig Game."""
+
 __all__ = ['dice', 'game', 'player']
